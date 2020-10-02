5dd09d86bd06f991cf057f908ae1991208eeb374
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2020-10-02 13:06:40 +0300

    Додає файл

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('А родители разрешили?');
+//   }
+// }
+
+// let age = prompt('Сколько вам лет?');
+
+// if (checkAge(age)) {
+//   alert('Доступ получен');
+// } else {
+//   alert('Доступ закрыт');
+// }
